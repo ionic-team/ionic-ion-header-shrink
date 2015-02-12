9f8735e7cb7b88c39cba2c3537ab2f8debc6f9ca
Vitaly Rotari <cky.adc@gmail.com>
2015-02-12 21:09:04 +0200

    Update ion-content top position

--- ionic.headerShrink.js
+++ ionic.headerShrink.js
@@ -7,24 +7,38 @@ angular.module('ionic.ion.headerShrink', [])
       var y = 0;
       var prevY = 0;
       var scrollDelay = 0.4;
+      var lastContentTop = 0;
 
       var fadeAmt;
       
+      var element = $element[0];
       var header = $element.parent()[0].querySelector('.bar-header');
       var headerHeight = header.offsetHeight;
       
       function onScroll(e) {
         var scrollTop = e.detail.scrollTop;
+        var contentTop = 0;
 
         if(scrollTop >= 0) {
           y = Math.min(headerHeight / scrollDelay, Math.max(0, y + scrollTop - prevY));
+          contentTop = headerHeight - y;
+          
+          if (contentTop < 0) {
+            contentTop = 0;
+          }
         } else {
           y = 0;
+          contentTop = headerHeight;
         }
 
         ionic.requestAnimationFrame(function() {
           fadeAmt = 1 - (y / headerHeight);
           header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, ' + -y + 'px, 0)';
+          
+          if (contentTop !== lastContentTop) {
+            element.style.top = (lastContentTop = contentTop) + 'px';
+          }
+          
           for(var i = 0, j = header.children.length; i < j; i++) {
             header.children[i].style.opacity = fadeAmt;
           }
